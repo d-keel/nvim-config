69252b1c76984aa7900f31e12c2e76ba3e857e35
Dalton Keel <daltonkeel02@gmail.com>
2024-11-24 11:10:02 -0500

    Added statusline configs.

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,4 +1,4 @@
-require('gitsigns').setup {
+require('gitsigns').setup({
   signs = {
     add          = { text = '┃' },
     change       = { text = '┃' },
@@ -47,4 +47,5 @@ require('gitsigns').setup {
     row = 0,
     col = 1
   },
-}
+})
+vim.cmd "set statusline+=%{get(b:,'gitsigns_status','')}"
