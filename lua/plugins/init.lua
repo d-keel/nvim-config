b51487ade2363fe9a3118895418276856b7786ff
Dalton <daltonkeel02@gmail.com>
2024-12-08 15:02:50 -0500

    Updated init.lua.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,62 +1,49 @@
 return {
-  {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  dependencies = {
-                        {'nvim-lua/plenary.nvim'}
-                     }
-  },
+    {'catppuccin/nvim', priority = 1000},
 
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-
-  {
     'nvim-treesitter/nvim-treesitter',
-    --build = ':TSUpdate'
-  },
 
-  'mbbill/undotree',
+    'mbbill/undotree',
 
-  'lewis6991/gitsigns.nvim',
+    'lewis6991/gitsigns.nvim',
 
-  {
-      'neovim/nvim-lspconfig',
-  },
+    'neovim/nvim-lspconfig',
 
-  {
-      'hrsh7th/nvim-cmp',
-      dependencies = {
-                      'hrsh7th/cmp-nvim-lsp',
-                      'L3MON4D3/LuaSnip',
-                      'saadparwaiz1/cmp_luasnip'
-                     },
-      config = function()
-          local cmp = require('cmp')
-          cmp.setup({
-              snippet = {
-                  expand = function(args)
-                      require('luasnip').lsp_expand(args.body)
-                  end,
-              },
-              mapping = cmp.mapping.preset.insert({
-                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                  ['<C-Space>'] = cmp.mapping.complete(),
-                  ['<C-e>'] = cmp.mapping.abort(),
-                  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies =  {
+                            'hrsh7th/cmp-nvim-lsp',
+                            'L3MON4D3/LuaSnip',
+                            'saadparwaiz1/cmp_luasnip'
+                        },
+        opts = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                            expand = function(args)
+                                require('luasnip').lsp_expand(args.body)
+                            end,
+                          },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                 }),
                 sources = cmp.config.sources({
-                  { name = 'nvim_lsp' },
-                  { name = 'luasnip' },
-                }, {
-                  { name = 'buffer' },
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    }, {
+                    { name = 'buffer' },
                 })
-          })
-      end,
-  },
+            })
+        end,
+    },
 
-  {
+    {
       'williamboman/mason.nvim',
-      config = function()
+      opts = function()
           require('mason').setup({
               ensure_installed = {
                   'clangd',
@@ -67,33 +54,40 @@ return {
 
           })
       end,
-  },
+    },
 
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = {'mason.nvim'},
-    config = function()
-        require('mason-lspconfig').setup()
-        require('mason-lspconfig').setup_handlers({
-            function(server_name)
-                require('lspconfig')[server_name].setup({
-                    capabilities = require('cmp_nvim_lsp').default_capabilities(),
-                })
-            end,
-        })
-    end,
-  },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = {'mason.nvim'},
+        opts = function()
+            require('mason-lspconfig').setup()
+            require('mason-lspconfig').setup_handlers({
+                function(server_name)
+                    require('lspconfig')[server_name].setup({
+                        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                    })
+                end,
+            })
+        end,
+        },
 
-  {
-      'Julian/lean.nvim',
-      dependencies = {
-                        {'neovim/nvim-lspconfig'},
-                        {'nvim-lua/plenary.nvim'},
-                        {'nvim-cmp'}
-                     },
-      opts = {
-          mappings = true,
-      }
-  }
+    {
+        'nvim-telescope/telescope.nvim',
+	    dependencies =  { 
+                            'nvim-lua/plenary.nvim',
+                        }
+    },
+
+    {
+        'Julian/lean.nvim',
+        dependencies =  {
+                            'neovim/nvim-lspconfig',
+                            'nvim-lua/plenary.nvim',
+                            'nvim-cmp'
+                        },
+        opts =  {
+                    mappings = true,
+                }
+    }
 
 }
